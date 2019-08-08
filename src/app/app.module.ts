658a794f38746bb82c698a8ad42518f328609b93
rajesh rathod <rajesh.rathod@gsolc.com>
2019-08-08 12:57:00 +0530

    intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+         {path: 'login',component: LoginComponent}
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
